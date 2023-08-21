70dcd88863cc503b295400e77cdfaae27104e1cf
DeepakJavaDev <dzunzunkar@gmail.com>
2023-08-21 13:51:39 +0530

    ng-switch

--- src/app/directives-demo/directives-demo.component.ts
+++ src/app/directives-demo/directives-demo.component.ts
@@ -1,17 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-directives-demo',
   templateUrl: './directives-demo.component.html',
   styleUrls: ['./directives-demo.component.css']
 })
-export class DirectivesDemoComponent {
+export class DirectivesDemoComponent implements OnInit {
 
   blockStatus:boolean=false;
+  selectedProduct:string="";
+  
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    
+  }
 
 
   onBlockCreate(){
     this.blockStatus=true;
   }
 
+  toggleOnBlockCreate(){
+    this.blockStatus=!this.blockStatus;
+  }
+
+  getProduct(event:any){
+    console.log(event.target.value);
+    this.selectedProduct=event.target.value;
+  }
 }
