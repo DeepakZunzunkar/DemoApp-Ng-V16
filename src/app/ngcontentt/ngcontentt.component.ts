eef632c8566ff52953b5c52ee44b3302ee22ff2d
DeepakJavaDev <dzunzunkar@gmail.com>
2023-09-21 10:36:12 +0530

    use ng-container

--- src/app/ngcontentt/ngcontentt.component.ts
+++ src/app/ngcontentt/ngcontentt.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DesignUtilityService } from '../services/design-utility.service';
 
 @Component({
   selector: 'app-ngcontentt',
   templateUrl: './ngcontentt.component.html',
   styleUrls: ['./ngcontentt.component.css']
 })
-export class NgcontenttComponent {
+export class NgcontenttComponent implements OnInit{
+  
+  products:any[]=[];
+  status:boolean=false;
+
+  constructor(private designUtility: DesignUtilityService) { }
+
+  ngOnInit(): void {
+    this.products=this.designUtility.product;
+  }
 
 }
